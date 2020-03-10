6b3d39de951f4d5f3c0cf9b43aca878db15eab35
Michael C <mdcovarrubias@gmail.com>
2020-03-09 19:31:52 -0700

    Overload functions
    __setitem__
    __getitem__
    __contains__
    in order to set values or set value from custom dictionary

--- scope.py
+++ scope.py
@@ -4,18 +4,21 @@ class Scope(object):
         self.name = name
         self.parent = parent
         self.values = {}
-    
+
     def set_parent(self, parent):
         self.parent = parent
 
     def get_parent(self):
         return self.parent
 
-    def set_value(self, var, value):
+    def __setitem__(self, var, value):
         self.values[var] = value
-    
-    def get_value(self, var):
+
+    def __getitem__(self, var):
         return self.values[var]
+
+    def __contains__(self, key):
+        return key in self.values
 
     def __repr__(self):
         output = []
@@ -23,5 +26,5 @@ class Scope(object):
 
         for key, value in self.values.items():
             output.append('{key}:{value}'.format(key=key, value=value))
-            
+
         return '\n'.join(output)
