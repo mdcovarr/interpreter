f2b7eee894bc63646b84ae12273013901fa63ed4
Michael C <mdcovarrubias@gmail.com>
2020-03-10 21:40:18 -0700

    Add documentation to scope obeject

--- scope.py
+++ scope.py
@@ -6,25 +6,36 @@ class Scope(object):
         self.values = {}
 
     def set_parent(self, parent):
+        """
+        Function used to set the parent scope
+        :param parent:
+        :return None:
+        """
         self.parent = parent
 
     def get_parent(self):
+        """
+        Function used to get the parent scope
+        :return None:
+        """
         return self.parent
 
-    def __setitem__(self, var, value):
-        self.values[var] = value
+    def __setitem__(self, key, value):
+        """
+        Function used to override dict[key] = value function of dictionary
+        :param key:
+        :param value:
+        :return:
+        """
+        self.values[key] = value
 
     def __getitem__(self, var):
+        """
+        Function used to get value from dict
+        :param var:
+        :return: value from self.values dictionary
+        """
         return self.values[var]
 
     def __contains__(self, key):
         return key in self.values
-
-    def __repr__(self):
-        output = []
-        output.append('{name}\n'.format(name=self.name))
-
-        for key, value in self.values.items():
-            output.append('{key}:{value}'.format(key=key, value=value))
-
-        return '\n'.join(output)
