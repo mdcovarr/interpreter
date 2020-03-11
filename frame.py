d139524c5ebddd07dde6c1de41ca39e5ab748a48
Michael C <mdcovarrubias@gmail.com>
2020-03-10 22:22:44 -0700

    initialize scope of frame

--- frame.py
+++ frame.py
@@ -1,10 +1,25 @@
+from scope import Scope
+
 
 class Frame(object):
     def __init__(self, name, global_scope):
         self.name = name
-        self.current_scope = None
+        self.current_scope = self.create_new_scope(name, global_scope)
         self.scopes = []
         self.scopes.append(self.current_scope)
+
+    @staticmethod
+    def create_new_scope(name, global_scope):
+        """
+        Helper function to create new scope
+        :param name:
+        :param global_scope:
+        :return:
+        """
+        scope_name = '{0}_{1}'.format(name, 1)
+        curr_scope = Scope(scope_name, global_scope)
+
+        return curr_scope
 
     def add_scope(self):
         """
