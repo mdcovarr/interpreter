b6c312ecdf778e7fa0a4b85b081a782d742e5795
Michael C <mdcovarrubias@gmail.com>
2020-03-10 22:00:01 -0700

    Add base stack class to repo

---
+++ stack.py
@@ -0,0 +1,21 @@
+
+class Stack(object):
+    def __init__(self):
+        self.current_frame = None
+        self.frames = []
+
+    def add_frame(self, name, global_scope):
+        """
+        Function used to add a new frame
+        :param name:
+        :param global_scope:
+        :return None:
+        """
+        pass
+
+    def del_frame(self):
+        """
+        Function used to delete a frame
+        :return None:
+        """
+        pass
