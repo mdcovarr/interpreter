f1a4370b6666ddaa5fd6d834e8d296814f5ae95c
Michael C <mdcovarrubias@gmail.com>
2020-03-16 10:50:00 -0700

    Add new example files.
    Files will be utilized to run test on interpreter implementation.

---
+++ examples/example1.c
@@ -0,0 +1,6 @@
+int main() {
+    int y = 9;
+    int x = 3;
+
+    return x + y;
+}
---
+++ examples/example2.c
@@ -0,0 +1,9 @@
+int main() {
+    int count = 0;
+
+    for (int i = 0; i < 100; i += 1) {
+        count += 1;
+    }
+
+    return count;
+}
