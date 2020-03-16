532f330566d93174c3b540b0761f38712bf9548d
Michael C <mdcovarrubias@gmail.com>
2020-03-16 11:42:30 -0700

    Adding function to get output from my interpreter implementation.

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 """
 import os
 import glob
+from subprocess import check_call, check_output
+import subprocess
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 TEST_DIR  = 'examples'
@@ -13,11 +15,29 @@ def get_test_files(test_dir):
 
     return test_files
 
+def run_c_program(filename):
+    outfile = './testfile'
+
+    try:
+        check_call(['gcc', filename, '-o', outfile])
+        status = check_call(outfile, shell=True)
+        check_call(['rm', outfile])
+    except subprocess.CalledProcessError as e:
+        return e.returncode
+
+    return status
+
+def run_interpreter(filename):
+    try:
+        check_call(['python3', '__main__.py', '-f', filename])
+    except subprocess.CalledProcessError as e:
+        return e.returncode
+
 def handle_tests(file_list):
     for filename in file_list:
         c_output = run_c_program(filename)
         my_output = run_interpreter(filename)
-
+        print(my_output)
 
 def main():
     """
@@ -26,7 +46,7 @@ def main():
     # need to get all test files
     test_files = get_test_files(os.path.join(CWD, TEST_DIR, '*'))
 
-    print(test_files)
+    handle_tests(test_files)
 
 if __name__ == '__main__':
     main()
