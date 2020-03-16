b0e46ff7a2597a22c807b3d3d6ac2f298039c3a1
Michael C <mdcovarrubias@gmail.com>
2020-03-16 11:55:19 -0700

    Printing color output when running tests.

--- test.py
+++ test.py
@@ -38,13 +38,32 @@ def check_test(out, my_out):
         return True
     return False
 
+def print_red(status):
+    return "\033[91m {}\033[00m".format(status)
+
+def print_green(status):
+    return "\033[92m {}\033[00m".format(status)
+
 def handle_tests(file_list):
     print('\n\n ------------ [ Running Tests ] ------------\n\n')
+    i = 0
+
     for filename in file_list:
         c_output = run_c_program(filename)
         my_output = run_interpreter(filename)
 
+        filename = os.path.basename(filename)
         status = check_test(c_output, my_output)
+
+        if status:
+            exit_status = print_green('[pass]')
+        else:
+            exit_status = print_red('[fail]')
+
+        print('{0}: {1}\t{2}'.format(i, filename, exit_status))
+
+        i += 1
+
 
 def main():
     """
